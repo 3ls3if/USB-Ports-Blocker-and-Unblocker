d40cafd6c75c42c89b5173ca3a6acd34e2777b7c
r0han <83662612+3ls3if@users.noreply.github.com>
2022-09-11 11:10:23 +0530

    Add files via upload

---
+++ usb_blocker.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<dos.h>
+#include<string.h>
+
+int main()
+{
+	char user_option[3];
+	
+	printf("\nDo you want to disable usb ports on your computer? [yes/no]: ");
+	gets(user_option);
+	
+
+	if(strcmp(user_option, "yes")==0)
+	{
+		int isTrue = system("REG ADD HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\USBSTOR \/v Start \/t REG_DWORD \/d 4 \/f");
+		
+		if(isTrue==0)
+			puts("\n\nAll the usb ports has been blocked successfully.");
+
+		else
+			puts("\n\nYou must be an Administrator to perform this action.");
+			
+			
+		getch();
+		
+	}
+	else{
+		
+		puts("\n\nChoose the correct option. [yes/no]");
+		
+		getch();
+		
+	}
+
+	return 0;
+}
+
